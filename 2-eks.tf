42e2dd1ea49190055c63bf9967e32b5ade32e7e5
jin <djzepssa@gmail.com>
2023-12-08 17:59:42 +0900

    modifed error syntax

--- 2-eks.tf
+++ 2-eks.tf
@@ -56,11 +56,3 @@ module "eks" {
   }
 }
 
- manage_aws_auth_configmap = true
-  aws_auth_roles = [
-    {
-      rolearn  = module.eks_admins_iam_role.iam_role_arn
-      username = module.eks_admins_iam_role.iam_role_name
-      groups   = ["system:masters"]
-    },
-  ]
