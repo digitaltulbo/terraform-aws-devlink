35e92557f94b80e48576567718073d36846c884c
Jin Choi <127823969+digitaltulbo@users.noreply.github.com>
2023-12-19 15:47:52 +0900

    push

--- terraform-module/eks.tf
+++ terraform-module/eks.tf
@@ -79,26 +79,26 @@ module "eks" {
     "k8s.io/cluster-autoscaler/${var.cluster_name}" : "true"  }
 }
 
-# https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
+# # https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
 # data "aws_eks_cluster" "default" {
-#   name = module.eks.cluster_id
+#   name = "devlink"
 # }
 
 # data "aws_eks_cluster_auth" "default" {
-#   name = module.eks.cluster_id
+#   name = "devlink"
 # }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  # token                  = data.aws_eks_cluster_auth.default.token
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.default.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+#   # token                  = data.aws_eks_cluster_auth.default.token
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
-    command     = "aws"
-  }
-}
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+#     command     = "aws"
+#   }
+# }
 
 
 
