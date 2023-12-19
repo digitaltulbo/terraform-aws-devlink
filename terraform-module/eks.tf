770e4e1a9142dff6fc34f3604e001488480b23a0
Jin Choi <127823969+digitaltulbo@users.noreply.github.com>
2023-12-19 15:37:34 +0900

    delete

--- terraform-module/eks.tf
+++ terraform-module/eks.tf
@@ -84,9 +84,9 @@ data "aws_eks_cluster" "default" {
   name = module.eks.cluster_id
 }
 
-data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_id
-}
+# data "aws_eks_cluster_auth" "default" {
+#   name = module.eks.cluster_id
+# }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
