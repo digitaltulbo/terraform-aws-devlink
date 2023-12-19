f113d572e8c1fc2be0a98fc997aca44d12397446
Jin Choi <127823969+digitaltulbo@users.noreply.github.com>
2023-12-19 16:18:22 +0900

    1

--- terraform-module/eks.tf
+++ terraform-module/eks.tf
@@ -63,47 +63,11 @@ module "eks" {
     },
   ]
 
-  # node_security_group_additional_rules = {
-  #   ingress_allow_access_from_control_plane = {
-  #     type                          = "ingress"
-  #     protocol                      = "tcp"
-  #     from_port                     = 9443
-  #     to_port                       = 9443
-  #     source_cluster_security_group = true
-  #     description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
-  #   }
-  # }
 
   tags = {
     "k8s.io/cluster-autoscaler/enabled" : "true"
     "k8s.io/cluster-autoscaler/${var.cluster_name}" : "true"  }
 }
-
-# # https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
-# data "aws_eks_cluster" "default" {
-#   name = "devlink"
-# }
-
-# data "aws_eks_cluster_auth" "default" {
-#   name = "devlink"
-# }
-
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.default.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-#   # token                  = data.aws_eks_cluster_auth.default.token
-
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
-#     command     = "aws"
-#   }
-# }
-
-
-
-
-
 
 
 
