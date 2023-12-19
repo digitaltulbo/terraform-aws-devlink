0111b9463cf6319ac2cbd27c3d0e625855a6e59b
Jin Choi <127823969+digitaltulbo@users.noreply.github.com>
2023-12-19 14:27:35 +0900

    update

--- terraform-module/eks.tf
+++ terraform-module/eks.tf
@@ -18,36 +18,90 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
   
-  # 관리형 노드 그룹 사용 (기본 설정)
+  enable_irsa = true
+
   eks_managed_node_group_defaults = {
-    ami_type               = "AL2_x86_64" # 
-    disk_size              = 10           # EBS 사이즈
-    instance_types         = ["t2.small"]
-    # vpc_security_group_ids = [aws_security_group.additional.id]
-    vpc_security_group_ids = []
-		
-		# cluster-autoscaler에 사용 될 IAM 등록
-    iam_role_additional_policies = ["${local.iam_role_policy_prefix}/${module.iam_policy_autoscaling.name}"]
+    disk_size = 50
   }
 
-  # 관리형 노드 그룹 사용 (노드별 추가 설정)
   eks_managed_node_groups = {
-    ("${var.cluster_name}-node-group") = {
-      # node group 스케일링
-      min_size     = 1 # 최소
-      max_size     = 3 # 최대
-      desired_size = 2 # 기본 유지
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
 
-      # 생성된 node에 labels 추가 (kubectl get nodes --show-labels로 확인 가능)
       labels = {
-        ondemand = "true"
+        role = "general"
       }
 
-      # 생성되는 인스턴스에 tag추가
-      tags = {
-        "k8s.io/cluster-autoscaler/enabled" : "true"
-        "k8s.io/cluster-autoscaler/${var.cluster_name}" : "true"
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
+
+    spot = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "spot"
       }
+
+      taints = [{
+        key    = "market"
+        value  = "spot"
+        effect = "NO_SCHEDULE"
+      }]
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "SPOT"
     }
   }
+
+  tags = {
+    Environment = "staging"
+  }
 }
+
+
+
+
+
+
+
+
+
+
+#   # 관리형 노드 그룹 사용 (기본 설정)
+#   eks_managed_node_group_defaults = {
+#     ami_type               = "AL2_x86_64" # 
+#     disk_size              = 10           # EBS 사이즈
+#     instance_types         = ["t2.small"]
+#     # vpc_security_group_ids = [aws_security_group.additional.id]
+#     vpc_security_group_ids = []
+		
+# 		# cluster-autoscaler에 사용 될 IAM 등록
+#     iam_role_additional_policies = ["${local.iam_role_policy_prefix}/${module.iam_policy_autoscaling.name}"]
+#   }
+
+#   # 관리형 노드 그룹 사용 (노드별 추가 설정)
+#   eks_managed_node_groups = {
+#     ("${var.cluster_name}-node-group") = {
+#       # node group 스케일링
+#       min_size     = 1 # 최소
+#       max_size     = 3 # 최대
+#       desired_size = 2 # 기본 유지
+
+#       # 생성된 node에 labels 추가 (kubectl get nodes --show-labels로 확인 가능)
+#       labels = {
+#         ondemand = "true"
+#       }
+
+#       # 생성되는 인스턴스에 tag추가
+#       tags = {
+#         "k8s.io/cluster-autoscaler/enabled" : "true"
+#         "k8s.io/cluster-autoscaler/${var.cluster_name}" : "true"
+#       }
+#     }
+#   }
+# }
