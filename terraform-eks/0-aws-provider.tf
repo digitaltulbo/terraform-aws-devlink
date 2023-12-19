7a5c46d01d0089833a62bc01901135f18d695eb4
Jin Choi <127823969+digitaltulbo@users.noreply.github.com>
2023-12-19 09:58:56 +0900

    version update

--- terraform-eks/0-aws-provider.tf
+++ terraform-eks/0-aws-provider.tf
@@ -3,11 +3,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
   region = "ap-northeast-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
+
