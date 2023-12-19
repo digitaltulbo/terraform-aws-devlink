1572033e70018be7757577f6999ea4333e8d2002
Jin Choi <127823969+digitaltulbo@users.noreply.github.com>
2023-12-19 13:36:03 +0900

    aa

--- terraform-module/variables.tf
+++ terraform-module/variables.tf
@@ -33,9 +33,3 @@ variable "vpc_name" {
   default = null
 }
 
-# terraform.auto.tfvars
-vpc_name     = "main-vpc"
-vpc_cidr     = "192.168.0.0/16"
-
-cluster_name     = "cluster-1"
-cluster_version  = "1.27"
