b5baa69d9f9a4cfadc3959a5415af87dc83b5806
Jin Choi <127823969+digitaltulbo@users.noreply.github.com>
2023-12-20 07:16:49 +0900

    iam

--- terraform-module/iam.tf
+++ terraform-module/iam.tf
@@ -78,55 +78,55 @@ module "eks_admins_iam_group" {
   custom_group_policy_arns          = [module.allow_assume_eks_admins_iam_policy.arn]
 }
 
-module "iam_policy_autoscaling" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
+# module "iam_policy_autoscaling" {
+#   source = "terraform-aws-modules/iam/aws//modules/iam-policy"
 
-  name = "${var.cluster_name}-cluster-autoscaler"
-  path = "/"
-  description = "Autoscaling policy for cluster ${var.cluster_name}"
+#   name = "${var.cluster_name}-cluster-autoscaler"
+#   path = "/"
+#   description = "Autoscaling policy for cluster ${var.cluster_name}"
 
-  policy      = data.aws_iam_policy_document.worker_autoscaling.json
-}
+#   policy      = data.aws_iam_policy_document.worker_autoscaling.json
+# }
 
-data "aws_iam_policy_document" "worker_autoscaling" {
-  statement {
-    sid    = "eksWorkerAutoscalingAll"
-    effect = "Allow"
+# data "aws_iam_policy_document" "worker_autoscaling" {
+#   statement {
+#     sid    = "eksWorkerAutoscalingAll"
+#     effect = "Allow"
 
-    actions = [
-      "autoscaling:DescribeAutoScalingGroups",
-      "autoscaling:DescribeAutoScalingInstances",
-      "autoscaling:DescribeLaunchConfigurations",
-      "autoscaling:DescribeTags",
-      "ec2:DescribeLaunchTemplateVersions",
-    ]
+#     actions = [
+#       "autoscaling:DescribeAutoScalingGroups",
+#       "autoscaling:DescribeAutoScalingInstances",
+#       "autoscaling:DescribeLaunchConfigurations",
+#       "autoscaling:DescribeTags",
+#       "ec2:DescribeLaunchTemplateVersions",
+#     ]
 
-    resources = ["*"]
-  }
+#     resources = ["*"]
+#   }
 
-  statement {
-    sid    = "eksWorkerAutoscalingOwn"
-    effect = "Allow"
+#   statement {
+#     sid    = "eksWorkerAutoscalingOwn"
+#     effect = "Allow"
 
-    actions = [
-      "autoscaling:SetDesiredCapacity",
-      "autoscaling:TerminateInstanceInAutoScalingGroup",
-      "autoscaling:UpdateAutoScalingGroup",
-    ]
+#     actions = [
+#       "autoscaling:SetDesiredCapacity",
+#       "autoscaling:TerminateInstanceInAutoScalingGroup",
+#       "autoscaling:UpdateAutoScalingGroup",
+#     ]
 
-    resources = ["*"]
+#     resources = ["*"]
 
-    condition {
-      test     = "StringEquals"
-      variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${var.cluster_name}"
-      values   = ["owned"]
-    }
+#     condition {
+#       test     = "StringEquals"
+#       variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${var.cluster_name}"
+#       values   = ["owned"]
+#     }
 
-    condition {
-      test     = "StringEquals"
-      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
-      values   = ["true"]
-    }
-  }
-}
+#     condition {
+#       test     = "StringEquals"
+#       variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
+#       values   = ["true"]
+#     }
+#   }
+# }
 
