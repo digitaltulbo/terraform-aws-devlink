10d7ee9cda4001c99ccfa7409029b73d088c1b11
digitaltulbo <djzepssa@gmail.com>
2023-12-20 00:41:01 +0000

    lb-controller added

--- terraform-module/iam.tf
+++ terraform-module/iam.tf
@@ -78,16 +78,6 @@ module "eks_admins_iam_group" {
   custom_group_policy_arns          = [module.allow_assume_eks_admins_iam_policy.arn]
 }
 
-module "eks_admins_iam_group" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
-  version = "5.3.1"
-
-  name                              = "terraform-admin"
-  attach_iam_self_management_policy = false
-  create_group                      = true
-  group_users                       = [module.user1_iam_user.iam_user_name]
-  custom_group_policy_arns          = [module.allow_assume_eks_admins_iam_policy.arn]
-}
 # module "iam_policy_autoscaling" {
 #   source = "terraform-aws-modules/iam/aws//modules/iam-policy"
 
