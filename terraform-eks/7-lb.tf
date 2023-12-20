6437a530b714c9bace6856abffc8e8b4984f0e17
Jin Choi <127823969+digitaltulbo@users.noreply.github.com>
2023-12-20 11:00:04 +0900

    lb

--- terraform-eks/7-lb.tf
+++ terraform-eks/7-lb.tf
@@ -13,13 +13,13 @@ resource "aws_lb" "web-lb"{
 }
 # Web Load Balancer Listener
 resource "aws_lb_listener" "web-lb-listener" {
-  load_balancer_arn = web_lb.web_lb.arn
+  load_balancer_arn = aws_lb.web-lb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.web_lb_tg.arn
+    target_group_arn = aws_lb_target_group.web-lb-tg.arn
   }
 }
 
