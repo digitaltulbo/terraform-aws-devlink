db8e81871b9adcac176ab89f10dfc746fb052c15
Jin Choi <127823969+digitaltulbo@users.noreply.github.com>
2023-12-20 11:12:39 +0900

    1w

--- terraform-eks/0-main.tf
+++ terraform-eks/0-main.tf
@@ -12,24 +12,5 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_name
-}
 
-data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.default.token
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
-}
 
