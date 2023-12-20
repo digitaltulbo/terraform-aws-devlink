b68356402a2248bc21e1330dceffaf2377343571
digitaltulbo <djzepssa@gmail.com>
2023-12-20 00:53:22 +0000

    helm

--- terraform-module/helm.tf
+++ terraform-module/helm.tf
@@ -1,10 +1,13 @@
+resource "aws_eks_cluster" "devlink" {
+  name     = var.cluster_name
+}
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.certificate_authority[0].data)
+    host                   = aws_eks_cluster.devlink.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.devlink.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.id]
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name.id]
       command     = "aws"
     }
   }
